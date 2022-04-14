c86b221f66f8c9c4b11649ef4c9a48e44fd7bfa3
Joseph Diza <josephm.diza@gmail.com>
2022-04-14 16:44:52 -0700

    Add initial setup.sh script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+openmaptiles_tools_installed=$(command -v download-osm)
+if [[ -z "$openmaptiles_tools_installed" ]]; then
+    # Install download-osm script
+    python3 -m pip install git+https://github.com/openmaptiles/openmaptiles-tools
+fi
+
+# Install dependencies with pacman
+pacman=$(command -v pacman)
+if [[ ! -z "$pacman" ]]; then
+    sudo pacman -S graphviz sqlite3 aria2
+    yay=$(command -v yay)
+    if [[ ! -z "$yay" ]]; then
+        yay -S osmtools
+    fi
+fi
+
+# Install dependencies with apt
+apt=$(command -v apt)
+if [[ ! -z "$apt" ]]; then
+    sudo apt install -y graphviz sqlite3 aria2 osmctools
+fi
+
+download-osm geofabrik 'north-america/canada/british-columbia'
