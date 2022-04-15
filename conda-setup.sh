f8f0cc220578a79f570279fbdad64fb48bdb7cd0
Joseph Diza <josephm.diza@gmail.com>
2022-04-14 17:24:47 -0700

    Refactor initial env setup

--- conda-setup.sh
+++ conda-setup.sh
@@ -2,14 +2,12 @@
 
 # Make sure conda/mamba is initialized first
 mamba=$(command -v mamba)
+conda="conda"
 if [[ ! -z "$mamba" ]]; then
-    mamba create --name poiota
-    mamba activate poiota
-    mamba install -c conda-forge pyrosm
-    mamba install -c conda-forge python-language-server
-else
-    conda create --name poiota
-    conda activate poiota
-    conda install -c conda-forge pyrosm
-    conda install -c conda-forge python-language-server
+    conda="mamba"
 fi
+
+$conda create --name poiota
+$conda activate poiota
+$conda install -c conda-forge pyrosm
+$conda install -c conda-forge python-language-server
